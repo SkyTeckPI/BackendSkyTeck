423c9d4fcd51c2de869de839e2d3e8a7504f691d
EyaCode <72935999+eya118@users.noreply.github.com>
2024-03-25 22:33:02 +0100

    master

---
+++ src/main/java/com/example/backendskyteck/BackendSkyTeckApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendskyteck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSkyTeckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSkyTeckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendskyteck/BackendSkyTeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendskyteck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSkyTeckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
